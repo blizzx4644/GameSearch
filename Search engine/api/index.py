97b29e87bf372dc8f2e4de98254927d885ed0219
Blizz <blizzx4644@gmail.com>
2026-03-30 20:38:46 +0200

    Update index.py to modify Python import path
    Add parent directory to Python path for imports

--- Search engine/api/index.py
+++ Search engine/api/index.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+# Ajouter le répertoire parent au chemin Python pour les imports
+sys.path.append(str(Path(__file__).parent.parent))
+
 from backend.app import app
 
 __all__ = ["app"]
